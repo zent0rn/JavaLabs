cc0171a22b4c644447e9df73b461ebfeac0898a6
salex <test@test.test>
2024-09-03 22:17:23 +0300

    Implement the method 'readNumbers'

--- src/main/java/webProgramming/lab1/InputOutputTool.java
+++ src/main/java/webProgramming/lab1/InputOutputTool.java
@@ -8,5 +8,13 @@ public class InputOutputTool {
    public InputOutputTool(){
        reader = new Scanner(System.in);
    }
-
+   
+    public int[] readNumbers(){
+        int n = reader.nextInt();
+        int[] buff = new int[n];
+        for(int i = 0; i < n; i++){
+            buff[i] = reader.nextInt();
+        }
+        return buff;
+    }
 }
