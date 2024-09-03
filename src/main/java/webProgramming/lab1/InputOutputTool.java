3a359103c042fc49bc859b2bf97c6fdd971293a0
salex <test@test.test>
2024-09-03 22:24:22 +0300

    Format the code of InputOutputTool

--- src/main/java/webProgramming/lab1/InputOutputTool.java
+++ src/main/java/webProgramming/lab1/InputOutputTool.java
@@ -3,25 +3,25 @@ package webProgramming.lab1;
 import java.util.Scanner;
 
 public class InputOutputTool {
-   private Scanner reader;
+    private final Scanner reader;
 
-   public InputOutputTool(){
-       reader = new Scanner(System.in);
-   }
+    public InputOutputTool() {
+        reader = new Scanner(System.in);
+    }
 
-    public int[] readNumbers(){
+    public int[] readNumbers() {
         int n = reader.nextInt();
         int[] buff = new int[n];
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             buff[i] = reader.nextInt();
         }
         return buff;
     }
 
-    public void showNumbers(int[] buff){
-       for(int i: buff){
-           System.out.print(i + " ");
-       }
-       System.out.println();
+    public void showNumbers(int[] buff) {
+        for (int i : buff) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
