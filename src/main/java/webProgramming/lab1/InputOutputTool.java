f62001622f6f590a65e6e407e3c5721c21b5748b
salex <test@test.test>
2024-09-03 22:14:01 +0300

    Create InputOutputTool class
    InputOutputTool allows client to read data from
    System.in and show data via System.out

---
+++ src/main/java/webProgramming/lab1/InputOutputTool.java
@@ -0,0 +1,12 @@
+package webProgramming.lab1;
+
+import java.util.Scanner;
+
+public class InputOutputTool {
+   private Scanner reader;
+
+   public InputOutputTool(){
+       reader = new Scanner(System.in);
+   }
+
+}
