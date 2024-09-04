65b46e9b6b94d0fdbcede971dec392e0ce434b27
salex <test@test.test>
2024-09-04 16:46:44 +0300

    Implement a "Solve" class

--- src/main/java/webProgramming/lab1/Solve.java
+++ src/main/java/webProgramming/lab1/Solve.java
@@ -1,5 +1,42 @@
 package webProgramming.lab1;
 
+import java.util.Arrays;
+
 public class Solve {
-    
+    private int[] data;
+    private final InputOutputTool iot;
+
+    public Solve() {
+        iot = new InputOutputTool();
+    }
+
+    public void readNumbers() {
+        data = iot.readNumbers();
+    }
+
+    public void displayHappyNumbers() {
+        int[] temp = new int[data.length];
+        int tempInd = 0;
+        for (int i : data) {
+            if (checkNumber(i)) {
+                temp[tempInd++] = i;
+            }
+        }
+        iot.showNumbers(Arrays.copyOf(temp, tempInd));
+    }
+
+    private boolean checkNumber(int number) {
+        /*
+        Число называется счастливым, если сумма квадратов его цифр кратна 4
+         */
+        int copyOfNumber = number;
+        int sum = 0;
+        while (copyOfNumber > 0) {
+            int currFigure = copyOfNumber % 10;
+            sum += currFigure * currFigure;
+            copyOfNumber /= 10;
+        }
+        return (sum % 4 == 0);
+    }
+
 }
