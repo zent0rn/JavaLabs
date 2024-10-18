b2558418a4e21875d6d63b0638a35ad439f2d997
Alexey Sazonov <salex.9206@mail.ru>
2024-10-18 20:31:33 +0300

    refactor: replace array of doubles with list in RegularHexagon

--- src/main/java/webProgramming/lab3v1/shapes/concrete/RegularHexagon.java
+++ src/main/java/webProgramming/lab3v1/shapes/concrete/RegularHexagon.java
@@ -2,25 +2,26 @@ package webProgramming.lab3v1.shapes.concrete;
 
 import webProgramming.lab3v1.shapes.Shape;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class RegularHexagon extends Shape {
     private static final int COUNT_SIDES = 6;
-    public RegularHexagon(String nameOfShape, double[] lengthOfSides) {
+
+    public RegularHexagon(String nameOfShape, List<Double> lengthOfSides) {
         super(nameOfShape, COUNT_SIDES, lengthOfSides);
     }
 
-    public static RegularHexagon of(String nameRegularHexagon, int side) {
+    public static RegularHexagon of(String nameRegularHexagon, double side) {
         if (nameRegularHexagon.isBlank()) {
             throw new IllegalArgumentException("Название не может быть пустым!");
         }
 
-        return new RegularHexagon(nameRegularHexagon, new double[]{side, side, side, side, side, side});
+        return new RegularHexagon(nameRegularHexagon, List.of(side, side, side, side, side, side));
     }
 
     @Override
     public double calculateSquare() {
-        return (3 * Math.pow(_lengthOfSides[0], 2) * Math.sqrt(3) / 2);
+        return (3 * Math.pow(_lengthOfSides.get(0), 2) * Math.sqrt(3) / 2);
     }
 
     @Override
@@ -28,7 +29,7 @@ public class RegularHexagon extends Shape {
         StringBuilder repr = new StringBuilder();
         repr.append("RegularHexagon{_nameOfShape='").append(_nameOfShape).append('\'').append(", _square=")
                 .append(_square).append(", _perimeter=").append(_perimeter).append(", _countOfSides=")
-                .append(_countOfSides).append(", _lengthOfSides=").append(Arrays.toString(_lengthOfSides)).append('}');
+                .append(_countOfSides).append(", _lengthOfSides=").append(_lengthOfSides.toString()).append('}');
         return repr.toString();
     }
 }
