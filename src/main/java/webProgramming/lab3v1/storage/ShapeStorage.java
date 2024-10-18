05f8517076472c048d686e36a18d1d78ef580173
Alexey Sazonov <salex.9206@mail.ru>
2024-10-18 20:22:15 +0300

    feat: create ShapeStorage

---
+++ src/main/java/webProgramming/lab3v1/storage/ShapeStorage.java
@@ -0,0 +1,30 @@
+package webProgramming.lab3v1.storage;
+
+import webProgramming.lab3v1.shapes.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeStorage {
+    private List<Shape> storage;
+
+    public ShapeStorage() {
+        storage = new ArrayList<>();
+    }
+
+    public void addShape(Shape shape) {
+        storage.add(shape);
+    }
+
+    public Shape getShape(int index) {
+        return storage.get(index);
+    }
+
+    public void removeShapeByIndex(int index) {
+        storage.remove(index);
+    }
+
+    public void removeShapeByObject(Shape shape) {
+        storage.remove(shape);
+    }
+}
